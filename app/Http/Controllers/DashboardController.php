e90ca907de365d829d84b4846e215c5a3b468d64
Jangbe <jangebe91@gmail.com>
2024-04-03 21:50:14 +0700

    Menambahkan dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Kendaraan;
+use App\Models\UjiEmisi;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
-    public function index() {
-        return view('uji-emisi.index'); 
-        // sementara, dashboard diarahkan ke uji emisi seabagi halaman utama
+    public function index()
+    {
+        $bengkel = User::count();
+        $kendaraan = Kendaraan::count();
+        $ujiEmisi = UjiEmisi::count();
+        $avg = UjiEmisi::selectRaw('sum(co) as CO, sum(hc) as HC, sum(opasitas) as Opasitas')->first();
+        $averages = [];
+        $max = ['CO' => 10, 'HC' => 1200, 'Opasitas' => 70];
+        foreach ($avg->toArray() as $l => $a) {
+            $averages[] = [
+                'label' => $l,
+                'value' => number_format($a / $ujiEmisi, 2),
+                'max' => $max[$l],
+                'color' => 'primary'
+            ];
+        }
+        $lastUjiEmisi = UjiEmisi::orderBy('id', 'desc')->take(10)->get();
+        return view('dashboard', compact('bengkel', 'kendaraan', 'ujiEmisi', 'averages', 'lastUjiEmisi'));
     }
 }
